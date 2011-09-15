f03587a180e7d80f42db05a38b2173f13babc9e2
Célio Fonseca <celio.fonseca@gmail.com>
2011-09-15 17:07:29 -0300

    Transformada a solucao para Orientacao a Objeto.

--- main.rb
+++ main.rb
@@ -1,27 +1,66 @@
-def sequencia_doida num, index  
-  #print "#{num} "
+class CalculatorCrazySequence
   
-  if num != 1
-    if num.odd?
-      index = sequencia_doida 3*num+1, index+1
+  attr_writer :number
+  
+  def initialize number = 1
+    @number = number
+  end
+  
+  def calculate_recursive number = @number, qntd = 1
+    if number != 1
+      if number.odd?
+        qntd = calculate_recursive 3*number+1, qntd+1
+      else
+        qntd = calculate_recursive number/2, qntd+1
+      end
+    end
+    
+    qntd
+  end
+  
+  def calculate_iterative
+    qntd = 1
+    
+    while @number > 1
+      if @number.odd?
+        @number = 3*@number+1
+      elsif
+        @number = @number/2
+      end
+      
+      qntd += 1
+    end
+    
+    qntd
+  end
+  
+  def calculate algorithm
+    if algorithm == :calculate_recursive
+      self.calculate_recursive
     else
-      index = sequencia_doida num/2, index+1
+      self.calculate_iterative
     end
   end
   
-  index
 end
+
+
 
 max = 0
 numero = 0
 
+calculator = CalculatorCrazySequence.new
+
 #1000000.times do |i| #tem que usar i+1
-for i in 1..1000000
-  qntd = sequencia_doida i, 1
+for i in 1..1_000_000
+  calculator.number = i
+  qntd = calculator.calculate :calculate_iterative
+  
   if qntd >= max
     max = qntd
     numero = i
   end
+  
 end
 
 puts "Numero: #{numero} - #{max}"
